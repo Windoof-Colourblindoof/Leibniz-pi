a6a659ee829fe973c1d5d61cfe58d30cd3286f76
Windoof-Colourblindoof <57870403+Windoof-Colourblindoof@users.noreply.github.com>
2021-02-10 15:51:08 +0100

    Reworked Code

--- Leibnizreihe.cs
+++ Leibnizreihe.cs
@@ -8,26 +8,43 @@ namespace Leibnizreihe
     {
         static void Main()
         {
+            Console.Title = "Leibniz formula for Pi in C#";
+            decimal Pi;
             while (true)
             {
-                decimal Sum = 0;
-                decimal Pi;
-                long Iterations;
-                long UserInput;
-                Console.Title = "Leibniz formula for Pi in C#";
-                Console.WriteLine("How many iterations should be executed?");
-                UserInput = Convert.ToInt64(Console.ReadLine());
-                Console.WriteLine("Pi is getting approximated, please wait...");
-                var Stopwatch = System.Diagnostics.Stopwatch.StartNew();
-                for (Iterations = 0; Iterations < UserInput; Iterations++)
-                {
-                    Sum += DecimalEx.Pow(-1, Iterations) / (2 * Iterations + 1);
-                }
-                Pi = Sum * 4;
-                Stopwatch.Stop();
-                Console.WriteLine("Pi = " + Pi + "\nWith " + Iterations.ToString("N0", new CultureInfo("de-DE")) + " added fractions.");
-                Console.WriteLine("Algorithm was executed in: " + Stopwatch.ElapsedMilliseconds.ToString("N", new CultureInfo("de-DE")) + " milliseconds.");
+                decimal UserInput = GetInput();
+                Pi = CalPi(UserInput);
+                Console.WriteLine("Pi = " + Pi + "\n");
             }
+        }
+
+        private static decimal GetInput()
+        {
+            decimal Input;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("How many iterations should be executed?");
+                    Input = Convert.ToInt64(Console.ReadLine());
+                    return Input;
+                }
+                catch
+                {
+                    Console.WriteLine("Your input was not valid, please try again.\n");
+                }
+            }
+        }
+
+        private static decimal CalPi(decimal Input)
+        {
+            decimal Pi = 0;
+            Console.WriteLine("Pi is getting approximated, please wait...\n");
+            for (decimal Iterations = 0; Iterations < Input; Iterations++)
+            {
+                Pi += DecimalEx.Pow(-1, Iterations) / (2 * Iterations + 1);
+            }
+            return Pi * 4;
         }
     }
 }
